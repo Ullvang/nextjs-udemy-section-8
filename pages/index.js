425bbe0879594921663dc230bc24efdce6d49593
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2021-03-11 17:54:32 +0100

    #138

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,32 @@
+import { useRef } from "react";
+
 export default function Home() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <br />
+          <input ref={emailInputRef} type="email" id="email" />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <br />
+          <textarea ref={feedbackInputRef} id="feedback" rows="5"></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
 }
